2b2c4cf0e93c9032e8a7af737a148bed70f22287
Robin <robinlievens24@gmail.com>
2016-06-04 16:51:24 +0200

    Add working zone check, reformat of stuff

--- check.py
+++ check.py
@@ -1,19 +1,30 @@
-def checkEmpty(row,col,sudoku):
+def empty(x,y,sudoku):
 	for number in sudoku:
-		if sudoku[row][col] == EMPTY:
+		if sudoku[x][y] == EMPTY:
 			return True
 
-def checkRowSafe(col,sudoku,n):
+def xSafe(y,sudoku,n):
 	for i in range(0,9):
-		if sudoku[i][col] == n:
+		if sudoku[y][i] == n:
 			return False
 	return True
 
-def checkColSafe(row,sudoku,n):
+def ySafe(x,sudoku,n):
 	for i in range(0,9):
-		if sudoku[row][i] == n:
+		if sudoku[i][x] == n:
 			return False
 	return True
 
-def checkFieldSafe(row,col,sudoku,n):
+def zoneSafe(x,y,sudoku,n):
+	zoneX, zoneY = 3*int(x/3), 3*int(y/3)
+	for yc in range(zoneY, zoneY+3):
+		for xc in range(zoneX, zoneX+3):
+			if sudoku[yc][xc] == n:
+				print('X%i Y%i = %i within the zone'%xc,yc,n)
+				return False
 	return True
+
+def safe(x,y,sudoku,n):
+	if xSafe(y,sudoku,n) and ySafe(x,sudoku,n) and zoneSafe(x,y,sudoku,n):
+		return True
+	return False
--- sudokusolver.py
+++ sudokusolver.py
@@ -1,24 +1,18 @@
 import check
 
-EMPTY = 0
-sudoku = [[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0]]
-	
-def checkSafe(row,col,sudoku,n):
-	if check.checkRowSafe(col,sudoku,n) and check.checkColSafe(row,sudoku,n) and check.checkFieldSafe:
-		return True
-	else:
-		return False
+EMPTY = 0 #0 1 2 3 4 5 6 7 8
+sudoku = [[0,0,0,0,0,0,0,0,0],# 0
+					[0,0,0,0,0,0,0,0,0],# 1
+					[0,0,0,0,0,0,0,0,0],# 2
+					[0,0,0,0,0,0,0,0,0],# 3
+					[0,0,0,0,0,0,0,0,0],# 4
+					[0,0,0,0,0,0,0,0,0],# 5
+					[0,0,0,0,0,0,0,0,0],# 6
+					[0,0,0,0,0,0,0,0,0],# 7
+					[0,0,0,0,0,0,0,0,0]]# 8
 
 
-if checkSafe(1,0,sudoku,1):
+if check.safe(1,6,sudoku,1): #sudoku[Y][X] but check.safe(<X>,<Y>,<SUDOKULIST>,<N>)
 	print('Safe!')
 else:
 	print('Not safe')
