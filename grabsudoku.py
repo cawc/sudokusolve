eb517bfedb91091f6af934448b8fe5caeed18211
Robin <robinlievens24@gmail.com>
2016-06-05 14:37:13 +0200

    Add ability to download multiple sudoku's at once

--- grabsudoku.py
+++ grabsudoku.py
@@ -1,18 +1,26 @@
 from bs4 import BeautifulSoup
 import requests, re
 
+n = int(input('How many sudoku\'s do you want to download (between 1 and 10)? '))
+if n < 1 or n > 10:
+	die()
+
 url = 'http://show.websudoku.com/?level=4'
-page = requests.get(url)
-page.raise_for_status()
-rawPage=page.text
 
-sudokuid = int(re.search(r'\d+', rawPage.split('\n')[20]).group())
+for i in range(n):
+	page = requests.get(url)
+	page.raise_for_status()
+	rawPage=page.text
 
-soup = BeautifulSoup(rawPage,'html.parser')
-sudokuTable = soup.findAll(True, {'class':['s0', 'd0']})
-sudoku = [ [(int(item['value']) if item.get('class')[0] == 's0' else 0) for item in sudokuTable][i:i+9] for i in range(0, 81, 9) ]
+	sudokuid = int(re.search(r'\d+', rawPage.split('\n')[20]).group())
 
-filename = 'sudokus/sudoku_%i.txt'%sudokuid
-sudokufile = open(filename, 'w')
-for line in sudoku:
-	sudokufile.write( str(line).replace(',',' ').replace('[','').replace(']',' ') + '\n' )
+	soup = BeautifulSoup(rawPage,'html.parser')
+	sudokuTable = soup.findAll(True, {'class':['s0', 'd0']})
+	sudoku = [ [(int(item['value']) if item.get('class')[0] == 's0' else 0) for item in sudokuTable][i:i+9] for i in range(0, 81, 9) ]
+
+	filename = 'sudokus/sudoku_%i.txt'%sudokuid
+	sudokufile = open(filename, 'w')
+	for line in sudoku:
+		sudokufile.write( str(line).replace(',',' ').replace('[','').replace(']',' ') + '\n' )
+
+input('Done!')
